a37195bfb2c0636549994840fa8e340c3e5d3591
Rhuan <rhuancarlos.queiroz@gmail.com>
2019-04-18 11:58:40 -0300

    Criada a classe canoa e implementado alguns de seus metodos

---
+++ inc/Canoa.hpp
@@ -0,0 +1,15 @@
+//
+// Created by rhuan on 18/04/19.
+//
+
+#ifndef EP1_CANOA_HPP
+#define EP1_CANOA_HPP
+
+#include "Embarcacao.hpp"
+
+class Canoa : public Embarcacao{
+public:
+    void defender() override;
+};
+
+#endif //EP1_CANOA_HPP
---
+++ src/Canoa.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rhuan on 18/04/19.
+//
+#include "Canoa.hpp"
+
+void Canoa::defender() {
+    get_corpo(0)->soma_vida(-1);
+}
+
